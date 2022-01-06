93ddd248cb0776893d0ab592b67e95fdf3a23310
Alyssa Voccia <avoccia@hotmail.com>
2022-01-06 12:01:36 -0500

    Updated the portfolio section to dynamically change when choosing different options

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,13 +1,15 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './portfolio.scss';
 
 import PortfolioList from '../portfolio-list/PortfolioList';
+import { featuredPortfolio, reactPortfolio, htmlCssJsPortfolio, treehousePortfolio } from '../../portfolio-data';
 
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("featured"); 
+  const [data, setData] = useState();
 
   const list = [
     {
@@ -26,7 +28,26 @@ export default function Portfolio() {
       id: "treehouse",
       title: "Treehouse Techdegree"
     }
-  ]
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case 'featured':
+        setData(featuredPortfolio);
+        break;
+      case 'react':
+        setData(reactPortfolio);
+        break;
+      case 'html-css-js':
+        setData(htmlCssJsPortfolio);
+        break;
+      case 'treehouse':
+        setData(treehousePortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected])
 
   return (
     <div className='portfolio' id='portfolio'>
@@ -37,109 +58,23 @@ export default function Portfolio() {
         ))}
       </ul>
       <div className="container">
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/lax-dashboard.png" alt="Lax Dashboard" />
-          </div>
-          <div class="card-body">
-            <span class="tag tag-react">React</span>
-            <h4>Lax Dashboard</h4>
-            <p>
-              Dashboard proejct created using React and Material UI, and Firebase. Users are able to login with authentication through Firebase and see how their metrics match up against others. This app is suited for college/club teams, as well as high school students who want to sign up and see how they match up with others across the country.
-            </p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
+        {data.map((data) => (
+          <div class="card">
+            <div class="card-header">
+              <img src={data.img} alt={data.alt} />
+            </div>
+            <div class="card-body">
+              <span class={`tag ${data.tag}`}>{data.tag === 'html-css-js' ? 'html/css/js' : data.tag}</span>
+              <h4>{data.title}</h4>
+              <p>{data.description}</p>
+              <div class="links">
+                <a className='card-link github' href={data.github}><i class="fab fa-github"></i>Github</a>
+                <a className='card-link' href={data.demo}>Demo</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/trainit.png" alt="Train It Log It" />
-          </div>
-          <div class="card-body">
-            <span class="tag tag-html-css-js">HTML/CSS/JS</span>
-            <h4>Train It Log It</h4>
-            <p>
-            Project created using JavaScript and Firebase that allows users to be able to sign up and record their workouts. Users are only able to enter the app once they have either signed up or logged into their existing account. It also includes a leaderboard so that users are able to see where they stand, the ability to add/remove friends.
-            </p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/crwn-clothing.png" alt="Crown Clothing" />
-          </div>
-          <div class="card-body">
-            <span class="tag tag-react">React</span>
-            <h4>Crwn Clothing</h4>
-            <p>
-            This project was completed during the Complete React Developer in 2022 course. Fictional clothing store created using React and Stripe. The user is able to sign up/sign in, view various directories, add items to their cart, and go through the checkout process.
-            </p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/employee-directory.JPG" alt="Employee Directory" />
-          </div>
-          <div class="card-body">
-            <div className="tags">
-              <span class="tag tag-treehouse">Treehouse</span>
-              <span class="tag tag-html-css-js">HTML/CSS/JS</span>
-            </div>
-            <h4>Employee Directory</h4>
-            <p>
-              This project uses Flexbox and Fetch API to create a fictional employee directory with a modal popup for more information about the employee.
-            </p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/photo-gallery.jpg" alt="Photo Gallery" />
-          </div>
-          <div class="card-body">
-            <div className="tags">
-              <span class="tag tag-treehouse">Treehouse</span>
-              <span class="tag tag-html-css-js">HTML/CSS/JS</span>
-            </div>
-            <h4>Photo Gallery</h4>
-            <p>
-              This project utilizes flexbox for a responsive layout the use of a jQuery plugin for a lightbox. The use of vanilla JavaScript allows the user to search/filter through the grid of images.
-            </p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
-            </div>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <img src="assets/style-guide.jpg" alt="Stlye Guide" />
-          </div>
-          <div class="card-body">
-            <div className="tags">
-              <span class="tag tag-treehouse">Treehouse</span>
-              <span class="tag tag-html-css-js">HTML/CSS/JS</span>
-            </div>
-            <h4>Style Guide</h4>
-            <p>A style guide created using Sass to help aid a user in the development of a website.</p>
-            <div class="links">
-              <a className='card-link github' href="https://github.com/alyssavoccia/lax-dashboard"><i class="fab fa-github"></i>Github</a>
-              <a className='card-link' href="https://alyssavoccia.github.io/lax-dashboard/">Demo</a>
-            </div>
-          </div>
-        </div>
+        ))}
+        
       </div>
   </div>
   )
--- src/portfolio-data.js
+++ src/portfolio-data.js
@@ -1,13 +1,241 @@
 export const featuredPortfolio = [
   {
     id: 1,
+    tag: 'react',
     title: 'Lax Dashboard',
     img: 'assets/lax-dashboard.png',
-    alt: 'Lax Dashboard'
+    alt: 'Lax Dashboard',
+    description: 'Dashboard proejct created using React and Material UI, and Firebase. Users are able to login with authentication through Firebase and see how their metrics match up against others. This app is suited for college/club teams, as well as high school students who want to sign up and see how they match up with others across the country.',
+    github: 'https://github.com/alyssavoccia/lax-dashboard',
+    demo: 'https://alyssavoccia.github.io/lax-dashboard/'
   },
   {
     id: 2,
+    tag: 'html-css-js',
     title: 'Train It Log It',
-    img: 'assets/trainit.png'
+    img: 'assets/trainit.png',
+    alt: 'Train It Log It',
+    description: 'Project created using JavaScript and Firebase that allows users to be able to sign up and record their workouts. Users are only able to enter the app once they have either signed up or logged into their existing account. It also includes a leaderboard so that users are able to see where they stand, the ability to add/remove friends.',
+    github: 'https://github.com/alyssavoccia/trainit-logit',
+    demo: 'https://alyssavoccia.github.io/trainit-logit'
+  },
+  {
+    id: 3,
+    tag: 'react',
+    title: 'Crwn Clothing',
+    img: 'assets/crwn-clothing.png',
+    alt: 'Crown Clothing',
+    description: 'This project was completed during the Complete React Developer in 2022 course. Fictional clothing store created using React and Stripe. The user is able to sign up/sign in, view various directories, add items to their cart, and go through the checkout process.',
+    github: 'https://github.com/alyssavoccia/crwn-clothing',
+    demo: 'http://crwn-clothing-testing.herokuapp.com/'
+  },
+  {
+    id: 4,
+    tag: 'treehouse',
+    title: 'Employee Directory',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Employee Directory',
+    description: 'This project uses Flexbox and Fetch API to create a fictional employee directory with a modal popup for more information about the employee.',
+    github: 'https://github.com/alyssavoccia/employee-directory',
+    demo: 'https://alyssavoccia.github.io/employee-directory/' 
+  },
+  {
+    id: 5,
+    tag: 'treehouse',
+    title: 'Photo Gallery',
+    img: 'assets/photo-gallery.jpg',
+    alt: 'Photo Gallery',
+    description: 'This project utilizes flexbox for a responsive layout the use of a jQuery plugin for a lightbox. The use of vanilla JavaScript allows the user to search/filter through the grid of images.',
+    github: 'https://github.com/alyssavoccia/photo-gallery',
+    demo: 'https://alyssavoccia.github.io/photo-gallery/'
+  },
+  {
+    id: 6,
+    tag: 'treehouse',
+    title: 'Style Guide',
+    img: 'assets/style-guide.jpg',
+    alt: 'Style Guide',
+    description: 'A style guide created using Sass to help aid a user in the development of a website.',
+    github: 'https://github.com/alyssavoccia/style-guide',
+    demo: 'https://alyssavoccia.github.io/style-guide/'
   }
-]
+];
+
+export const reactPortfolio = [
+  {
+    id: 1,
+    tag: 'react',
+    title: 'Lax Dashboard',
+    img: 'assets/lax-dashboard.png',
+    alt: 'Lax Dashboard',
+    description: 'Dashboard proejct created using React and Material UI, and Firebase. Users are able to login with authentication through Firebase and see how their metrics match up against others. This app is suited for college/club teams, as well as high school students who want to sign up and see how they match up with others across the country.',
+    github: 'https://github.com/alyssavoccia/lax-dashboard',
+    demo: 'https://alyssavoccia.github.io/lax-dashboard/'
+  },
+  {
+    id: 2,
+    tag: 'react',
+    title: 'Crwn Clothing',
+    img: 'assets/crwn-clothing.png',
+    alt: 'Crown Clothing',
+    description: 'This project was completed during the Complete React Developer in 2022 course. Fictional clothing store created using React and Stripe. The user is able to sign up/sign in, view various directories, add items to their cart, and go through the checkout process.',
+    github: 'https://github.com/alyssavoccia/crwn-clothing',
+    demo: 'http://crwn-clothing-testing.herokuapp.com/'
+  },
+  {
+    id: 3,
+    tag: 'react',
+    title: 'Digital Business Card',
+    img: 'assets/lax-dashboard.png',
+    alt: 'Digital Business Card',
+    description: 'This is a digital business card that I created using React. It includes some basic contact info, as well as background information.',
+    github: 'https://github.com/alyssavoccia/digital-business-card',
+    demo: 'https://alyssavoccia.github.io/digital-business-card/'
+  },
+  {
+    id: 4,
+    tag: 'react',
+    title: 'Expensify App',
+    img: 'assets/lax-dashboard.png',
+    alt: 'Expensify App',
+    description: 'This is an app that I created while completing a Complete React Developer course. It includes the ability for users to sign up/sign in and track their expenses, utilizing Firebase to authenticate users and store the data.',
+    github: 'https://github.com/alyssavoccia/react-course-2-expensify-app',
+    demo: 'https://react-expensifyapp.herokuapp.com/'
+  },
+  {
+    id: 5,
+    tag: 'react',
+    title: 'Monsters Rolodex',
+    img: 'assets/lax-dashboard.png',
+    alt: 'Monsters Rolodex',
+    description: 'This is a simple React project that creates a searchable directory of monster cards using Fetch API.',
+    github: 'https://github.com/alyssavoccia/monsters-rolodex',
+    demo: 'https://alyssavoccia.github.io/monsters-rolodex/'
+  }
+];
+
+export const htmlCssJsPortfolio = [
+  {
+    id: 1,
+    tag: 'html-css-js',
+    title: 'Train It Log It',
+    img: 'assets/trainit.png',
+    alt: 'Train It Log It',
+    description: 'Project created using JavaScript and Firebase that allows users to be able to sign up and record their workouts. Users are only able to enter the app once they have either signed up or logged into their existing account. It also includes a leaderboard so that users are able to see where they stand, the ability to add/remove friends.',
+    github: 'https://github.com/alyssavoccia/trainit-logit',
+    demo: 'https://alyssavoccia.github.io/trainit-logit'
+  },
+  {
+    id: 2,
+    tag: 'html-css-js',
+    title: 'VAV Customs',
+    img: 'assets/trainit.png',
+    alt: 'VAV Customs',
+    description: 'Landing page for VAV Customs, a woodworking company. Built using HTML5 and CSS Grid. JavaScript was used to be able to get the images from their folder and to implement the lightbox for the grid items.',
+    github: 'https://github.com/alyssavoccia/vav-customs',
+    demo: 'https://alyssavoccia.github.io/vav-customs/'
+  },
+  {
+    id: 3,
+    tag: 'html-css-js',
+    title: 'Seaside Equity Group',
+    img: 'assets/trainit.png',
+    alt: 'Seaside Equity Group',
+    description: 'Landing page for a real estate investment firm. Currently only has a link to subscribe to their newsletter.',
+    github: 'https://github.com/alyssavoccia/seaside-equity-group',
+    demo: 'https://alyssavoccia.github.io/seaside-equity-group/'
+  },
+  {
+    id: 4,
+    tag: 'html-css-js',
+    title: 'Clear View Escapes',
+    img: 'assets/trainit.png',
+    alt: 'Clear View Escapes',
+    description: 'This is a responsive website that I made through a class on Udemy. Through this class I learned more about HTML, CSS, and JavaScript. I also was able to learn the basics of git and how to use it to manage packages within a project.',
+    github: 'https://github.com/alyssavoccia/travel-site',
+    demo: 'https://alyssavoccia.github.io/travel-site/'
+  },
+  {
+    id: 5,
+    tag: 'html-css-js',
+    title: 'The Pig Game',
+    img: 'assets/trainit.png',
+    alt: 'The Pig Game',
+    description: "This is a game completed during The Complete JavaScript Course where 2 players to race to 100 points. The current score goes up on each dice roll until a player rolls a 1. Upon rolling a 1, the player will lose their current score and it will be the next player's turn. To keep their current score, the user can choose to hold and let the next player go.",
+    github: 'https://github.com/alyssavoccia/the-pig-game',
+    demo: 'https://alyssavoccia.github.io/the-pig-game/'
+  },
+  {
+    id: 6,
+    tag: 'html-css-js',
+    title: 'Guess My Number',
+    img: 'assets/trainit.png',
+    alt: 'Guess My Number',
+    description: 'This is a game completed during The Complete JavaScript Course that allows a user to try and guess a random #. The browser will lead the user in the right direction if they guess too high/low.',
+    github: 'https://github.com/alyssavoccia/guess-my-number',
+    demo: 'https://alyssavoccia.github.io/guess-my-number/'
+  }
+];
+
+export const treehousePortfolio = [
+  {
+    id: 1,
+    tag: 'treehouse',
+    title: 'Employee Directory',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Employee Directory',
+    description: 'This project uses Flexbox and Fetch API to create a fictional employee directory with a modal popup for more information about the employee.',
+    github: 'https://github.com/alyssavoccia/lax-dashboard',
+    demo: 'https://alyssavoccia.github.io/employee-directory/'
+  },
+  {
+    id: 2,
+    tag: 'treehouse',
+    title: 'Photo Gallery',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Photo Gallery',
+    description: 'This project utilizes flexbox for a responsive layout the use of a jQuery plugin for a lightbox. The use of vanilla JavaScript allows the user to search/filter through the grid of images.',
+    github: 'https://github.com/alyssavoccia/photo-gallery',
+    demo: 'https://alyssavoccia.github.io/photo-gallery/'
+  },
+  {
+    id: 3,
+    tag: 'treehouse',
+    title: 'Game Show',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Game Show',
+    description: "Game Show is an interactive JavaScript game that allows users to choose letters to guess a random phrase on the game board. Make too many wrong guesses and it's game over.",
+    github: 'https://github.com/alyssavoccia/game-show',
+    demo: 'https://alyssavoccia.github.io/game-show/'
+  },
+  {
+    id: 4,
+    tag: 'treehouse',
+    title: 'Style Guide',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Style Guide',
+    description: 'A style guide created using Sass to help aid a user in the development of a website.',
+    github: 'https://github.com/alyssavoccia/style-guide',
+    demo: 'https://alyssavoccia.github.io/style-guide/'
+  },
+  {
+    id: 5,
+    tag: 'treehouse',
+    title: 'Web App Dashboard',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Web App Dashboard',
+    description: 'This is a responsive and interactive dashboard, created using CSS Grid and JavaScript. Users are able to simulate seeing notifications, tabbing through different chart options, and various forms.',
+    github: 'https://github.com/alyssavoccia/web-app-dashboard',
+    demo: 'https://alyssavoccia.github.io/web-app-dashboard/'
+  },
+  {
+    id: 6,
+    tag: 'treehouse',
+    title: 'Online Registration',
+    img: 'assets/employee-directory.JPG',
+    alt: 'Online Registration',
+    description: 'This is an online form that utilizes HTML5 form elements and CSS Flexbox for the layout.',
+    github: 'https://github.com/alyssavoccia/online-registration',
+    demo: 'https://alyssavoccia.github.io/online-registration/'
+  }
+];
