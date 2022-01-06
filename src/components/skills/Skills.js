7c4d57989361dc5ceeb0804ba9f41d125d7f59f5
Alyssa Voccia <avoccia@hotmail.com>
2022-01-06 16:11:32 -0500

    Updated skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -5,6 +5,31 @@ export default function Skills() {
   return (
     <div className='skills' id='skills'>
       <h1>Skills</h1>
+      <div className="skill-lists">
+        <div className="skill-list">
+          <ul>
+            <li className="skill-list__item list-item-short">HTML5</li>
+            <li className="skill-list__item list-item-short">CSS3</li>
+            <li className="skill-list__item">JAVASCRIPT</li>
+            <li className="skill-list__item list-item-short">REACT</li>
+            <li className="skill-list__item">BOOTSTRAP</li>
+            <li className="skill-list__item list-item-short">SASS</li>
+          </ul>
+        </div>
+        <div className="skill-list">
+          <ul>
+            <li className="skill-list__item">NODE</li>
+            <li className="skill-list__item list-item-short">PHP</li>
+            <li className="skill-list__item list-item-long">MYSQL</li>
+          </ul>
+        </div>
+        <div className="skill-list">
+          <ul>
+            <li className="skill-list__item list-item-short">GIT</li>
+            <li className="skill-list__item list-item-long">GITHUB</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
