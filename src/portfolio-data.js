939bccec82f997c6e8ea27a3eae02b69c9cd01b0
Alyssa Voccia <avoccia@hotmail.com>
2022-04-30 12:58:20 -0400

    Updated portfolio and readme

--- src/portfolio-data.js
+++ src/portfolio-data.js
@@ -16,8 +16,8 @@ export const featuredPortfolio = [
     img: 'assets/portfolio-pics/vav-customs.png',
     alt: 'VAV Customs',
     description: 'This project was recently redone with React! It utilizes a landing page for the homepage, making use of CSS Grid. It also contains a blog & store page. While the blog page is currently under construction, the shop page utilizes the Shopify-Buy SDK to render shop items.',
-    github: 'https://github.com/alyssavoccia/crwn-clothing',
-    demo: 'http://crwn-clothing-testing.herokuapp.com/'
+    github: 'https://github.com/alyssavoccia/vav-customs-v2',
+    demo: 'http://vavcustoms.rf.gd/'
   },
   {
     id: 3,
@@ -268,7 +268,8 @@ export const clientsPortfolio = [
     img: 'assets/portfolio-pics/vav-customs.png',
     alt: 'VAV Customs',
     description: 'This project was recently redone with React! It utilizes a landing page for the homepage, making use of CSS Grid. It also contains a blog & store page. While the blog page is currently under construction, the shop page utilizes the Shopify-Buy SDK to render shop items.',
-    github: 'https://github.com/alyssavoccia/crwn-clothing',
-    demo: 'http://crwn-clothing-testing.herokuapp.com/'
+    github: 'https://github.com/alyssavoccia/vav-customs-v2',
+    demo: 'http://vavcustoms.rf.gd/'
   },
 ]
+
